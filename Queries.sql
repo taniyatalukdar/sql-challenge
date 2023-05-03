5956fd83a74d9e0e260daf5dd1815a2eb1888225
taniytalukdar <taniyatalukdar@gmail.com>
2023-05-03 11:37:59 -0400

    Pushing queries

--- Queries.sql
+++ Queries.sql
@@ -1,27 +1,66 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/ZPe0Gc
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
-
---List the employee number, last name, first name, sex, and salary of each employee.
+--List the employee number, last name, first name, sex, and salary of each employee. LEFT JOIN
+SELECT a.emp_no, a.first_name, a.last_name, a.sex, b.salary
+FROM employees as a
+JOIN salaries as b
+ON a.emp_no = b.emp_no;
 
 --List the first name, last name, and hire date for the employees who were hired in 1986.
+---->Simple
+SELECT a.emp_no, a.last_name, a.first_name, EXTRACT(YEAR FROM a.hire_date) hire_year 
+FROM employees a
+WHERE EXTRACT(YEAR FROM a.hire_date)=1986;
 
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT a.dept_no, b.dept_name,c.emp_no, c.last_name, c.first_name
+FROM dept_manager a 
+	INNER JOIN departments b
+		ON a.dept_no=b.dept_no
+	INNER JOIN employees c
+		ON a.emp_no=c.emp_no;
 
 --List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT a.dept_no, a.dept_name,c.emp_no, c.last_name, c.first_name
+FROM departments a 
+     INNER JOIN dept_emp as b
+	 ON a.dept_no=b.dept_no
+	 INNER JOIN employees as c
+	 ON b.emp_no=c.emp_no
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
 --List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
+FROM employees as a
+    JOIN dept_emp as b
+    ON a.emp_no=b.emp_no
+	JOIN departments as c
+	ON b.dept_no = c.dept_no
+	WHERE c.dept_name = 'Sales';
 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
+FROM employees as a
+    JOIN dept_emp as b
+    ON a.emp_no=b.emp_no
+	JOIN departments as c
+	ON b.dept_no = c.dept_no
+	WHERE c.dept_name = 'Sales'
+	OR c.dept_name = 'Development';
+
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
 
 
 
@@ -42,4 +81,6 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+
 
