c2eeac2ba937b42e573f6d67c82e7ba86379f935
taniytalukdar <taniyatalukdar@gmail.com>
2023-05-03 23:04:35 -0400

    Adding Readme

--- Queries.sql
+++ Queries.sql
@@ -1,14 +1,14 @@
 --List the employee number, last name, first name, sex, and salary of each employee. LEFT JOIN
 SELECT a.emp_no, a.first_name, a.last_name, a.sex, b.salary
 FROM employees as a
-JOIN salaries as b
-ON a.emp_no = b.emp_no;
+LEFT JOIN salaries as b
+ON a.emp_no = b.emp_no
+ORDER BY a.emp_no;
 
---List the first name, last name, and hire date for the employees who were hired in 1986.
----->Simple
-SELECT a.emp_no, a.last_name, a.first_name, EXTRACT(YEAR FROM a.hire_date) hire_year 
+--List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT a.last_name, a.first_name, a.hire_date
 FROM employees a
-WHERE EXTRACT(YEAR FROM a.hire_date)=1986;
+WHERE a.hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT a.dept_no, b.dept_name,c.emp_no, c.last_name, c.first_name
@@ -25,10 +25,11 @@ FROM departments a
 	 ON a.dept_no=b.dept_no
 	 INNER JOIN employees as c
 	 ON b.emp_no=c.emp_no
+ORDER BY c.emp_no	 
 
 --List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
-SELECT first_name, last_name
+SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
@@ -36,11 +37,13 @@ AND last_name LIKE 'B%';
 --List each employee in the Sales department, including their employee number, last name, and first name.
 SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
 FROM employees as a
-    JOIN dept_emp as b
+    INNER JOIN dept_emp as b
     ON a.emp_no=b.emp_no
-	JOIN departments as c
+	INNER JOIN departments as c
 	ON b.dept_no = c.dept_no
-	WHERE c.dept_name = 'Sales';
+	WHERE c.dept_name = 'Sales'
+ORDER BY a.emp_no;	
+
 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
@@ -49,8 +52,8 @@ FROM employees as a
     ON a.emp_no=b.emp_no
 	JOIN departments as c
 	ON b.dept_no = c.dept_no
-	WHERE c.dept_name = 'Sales'
-	OR c.dept_name = 'Development';
+	WHERE c.dept_name IN ('Sales', 'Development')
+ORDER BY a.emp_no;
 
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
---
+++ README.md
@@ -0,0 +1,39 @@
+OVERVIEW:
+This week?s Challenge is about the HR data in a company named Pewlett Hackard (a fictional company). 
+The task was to create a research project about people whom the company employed during the 1980s and 1990s. Six CSV files were provided as data to load and create 6 tables for a database namely: employees, departments, dept_manager, dept_emp, salaries and titles. The following ERD was used to build the database:
+
+OVERVIEW:
+
+
+
+ANALYSIS
+Resources used:
+I used PostgreSQL in PGAdmin4 to create the database and then the tables and eventually the queries. I used QuickDBD to create the ERD. 
+I worked with my tutor, Kelli Kennedy and a lot of internet searches.
+
+
+Most of the requirements needed joining different tables. For example, to find the list of employees in Sales and Development we needed to join three tables: employees, dept_emp and departments. The query looks like: SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
+FROM employees as a
+    JOIN dept_emp as b
+    ON a.emp_no=b.emp_no
+	JOIN departments as c
+	ON b.dept_no = c.dept_no
+	WHERE c.dept_name IN ('Sales', 'Development')
+ORDER BY a.emp_no;
+
+The result is:
+
+
+
+For analysis no. 5 - List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B. ? I used the following query:
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+The data was found in a single table. The following was the result:
+
+
+
+CONCLUSION:
+While we explored the data which is mostly for older employees, perhaps we should increase the scope to check the data about younger employees and which department employees the youngest employees. We can also check how long they have been in their current roles and with the organization.
